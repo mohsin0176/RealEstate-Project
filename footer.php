73ab753430ebb918ea9a9ef0752e0ae42d3d4227
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-15 10:07:42 -0700

    Files Are Modified

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+</body>
+</html>
